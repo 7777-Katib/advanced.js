03b2dd3ea6a0a7d16f75969c0c0885cb7f97e820
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-01-26 19:36:06 +0600

    git commit

---
+++ true-false.js
@@ -0,0 +1,33 @@
+ //Falsy Values: null,"",undefined,NaN,false er khetre false hobe
+ //Truthy Values: "0"," ",[],{},true
+ // "" er vitore false likhle tokhon ota string hobe..r sob string holo true
+ 
+ const age=4;
+
+ if (age){
+     console.log("condition is true");
+ }
+ else{
+     console.log("condition is false");
+ }
+
+// const name='solaiman';
+const name=0;
+
+if (name || name==0){
+     console.log("condition is true");
+ }
+ else{
+     console.log("condition is false");
+}
+
+
+let nam;
+console.log(nam);
+
+if (nam){
+     console.log("condition is true");
+ }
+ else{
+     console.log("condition is false");
+}
