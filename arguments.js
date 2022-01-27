43402681a7e409c6c793e118524f55eef63b1639
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-01-27 16:09:09 +0600

    fjfhj

---
+++ arguments.js
@@ -0,0 +1,7 @@
+function add(num1,num2){
+    //console.log([...arguments]);
+    return num1+num2+arguments[1];
+}
+
+const result=add(2,3,4,5);
+console.log(result);
---
+++ scope.js
@@ -0,0 +1,29 @@
+
+let bonus=20;//global scope...it can be accesed from anywhere
+
+function sum(first,second){
+    let result=first+second+bonus; //function scope cz function er vitore variable declare hoise..function er bahir theke call hobe na
+    console.log(result);
+    if (result>9){
+        const mood="happy";
+        console.log(mood); 
+    }
+    
+    return result;
+}
+
+const output=sum(3,7);
+
+console.log(output);
+
+
+r=9
+if(r==9){                 //{} er vitore const r let die die thakle seta block scope........var die if block a call korle seta automatically hoist hoye upore uthe jay
+    var l="djksj";
+    console.log(l);
+}
+console.log(l);
+
+
+console.log(f);
+var f="jdn";
